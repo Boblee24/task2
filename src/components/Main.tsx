a7485777c3a89d2849a9c51cf52e97e9210ba573
Aborisade <bobleeswaggs123@gmail.com>
2024-07-08 15:06:33 +0200

    commit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,17 +19,18 @@ const Main: React.FC = () => {
         return image;
       };
     return (
-        <div>
+        <div className='px-[2rem] w-full'>
             {foodData.foods.map((food: Food) => (
-                <div key={food.id} className="food-item">
+                <div key={food.id} className="my-[2rem] ">
                     <img src={imageFunc(food.image)} alt={food.name} className="food-image" />
                     <h2 className="text-[2rem] text-[#FFF078]">{food.name}</h2>
                     <p className="text-[#FFF078]">{food.description}</p>
-                    <p className="text-[#FFF078]">${food.price.toFixed(2)}</p>
-                    <button onClick={() => handleBuyNow(food.name)} className="bg-[#FFF078] flex items-center rounded-lg py-1 px-2">
+                    <p className="text-[#FFF078] font-semibold my-3 text-[1.2rem]">${food.price.toFixed(2)}</p>
+                    <button onClick={() => handleBuyNow(food.name)} className="bg-[#FFF078] flex items-center m-auto rounded-lg py-1 px-2">
                         <h3 className='text-[#FF0000]'>Buy Now</h3>
                         <img src={cart} className='fill-[red]' alt="" />
                     </button>
+                    <hr className='my-4'/>
                 </div>
             ))}
         </div>
