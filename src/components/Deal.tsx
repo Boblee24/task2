99319be6ce3809498f0ada9e06f9fca14c7ab537
Aborisade <bobleeswaggs123@gmail.com>
2024-07-09 02:04:57 +0200

    push

--- src/components/Deal.tsx
+++ src/components/Deal.tsx
@@ -1,7 +1,7 @@
 import whitecart from "../assets/whitcart.svg"
 const Deal = () => {
     return (
-        <div className="deal flex items-center my-8 justify-center">
+        <div className="deal flex items-center my-[4rem] justify-center">
             <div>
                 <h1 className="text-[#FF0000B0] text-[1.5rem] pb-3 text-center">Order Now to get this exclusive offers at 5% discount</h1>
                 <button className="flex bg-[#FF0000B0] p-2 justify-center items-center m-auto rounded-2xl">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ interface navProps {
 const Navbar: React.FC<navProps> = ({count}) => {
 
     return (
-        <div className='flex justify-around items-center'>
+        <div className='flex justify-around items-center fixed top-0 w-full bg-[#DA291C]'>
             <div>
                 <img src={logo} alt='logo' />
             </div>
